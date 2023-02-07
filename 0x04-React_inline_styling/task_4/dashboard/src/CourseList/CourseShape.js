ae23a56e227e040fb9fc3810c503d8cf8a038cb7
Alegbe Mojisola Yemisi <51059680+yemisi567@users.noreply.github.com>
2023-02-07 18:44:12 +0100

    fix beneficiray

---
+++ 0x04-React_inline_styling/task_4/dashboard/src/CourseList/CourseShape.js
@@ -0,0 +1,9 @@
+import propTypes from "prop-types";
+
+const CourseShape = propTypes.shape({
+  id: propTypes.number.isRequired,
+  name: propTypes.string.isRequired,
+  credits: propTypes.number.isRequired,
+});
+
+export default CourseShape;
